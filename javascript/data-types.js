8232c0eb6b0f44cda4020f08cc1e71e6492e47d2
Dheeraj <dheerajshrivastav98@gmail.com>
2026-03-18 20:22:19 +0530

    data-types questions practice

---
+++ javascript/data-types.js
@@ -0,0 +1,21 @@
+let my_fav_color = 'Black'
+console.log(my_fav_color)
+my_fav_color = 'White'
+console.log(my_fav_color)
+
+function block() {
+  if (true) {
+    console.log('inside the if block')
+    var bus_number = {
+      henjewadi: 100,
+    }
+    console.log(typeof bus_number)
+    console.log(bus_number)
+  }
+  console.log(bus_number)
+}
+block()
+// console.log(bus_number);
+
+let one = true
+console.log('Addition of the boolean and number is:', one - true)
