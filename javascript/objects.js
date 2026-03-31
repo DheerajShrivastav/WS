52c75f6e9e60bbcd574d95a50bace121c9716657
Dheeraj <dheerajshrivastav98@gmail.com>
2026-03-31 15:01:35 +0530

    question no 2

--- javascript/objects.js
+++ javascript/objects.js
@@ -12,3 +12,13 @@ student.grade = "B"
 console.log(student)
 delete student.age
 console.log(student)
+
+let Books = {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    details: {
+        pages: 180,
+        genre: "Novel"
+    }
+}
+console.log(Books.details)
